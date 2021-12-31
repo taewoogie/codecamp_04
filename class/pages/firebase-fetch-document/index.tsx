a61abff6c48c9f9f039c20f53ceb0380e7bebe44
kwontaewook <taewoogie@gmail.com>
2021-12-31 19:41:37 +0900

    firebase - add Modal

--- class/pages/firebase-fetch-document/index.tsx
+++ class/pages/firebase-fetch-document/index.tsx
@@ -1,8 +1,13 @@
 // Initialize Cloud Firestore through Firebase
+import { Modal } from "antd";
 import { initializeApp } from "firebase/app";
 import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { useState } from "react";
 
 const LoginUI = () => {
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
   const firebaseConfig = {
     apiKey: "AIzaSyB3FSnWLebiGYOvA-GlEbdDwPfMW8Tvq0M",
     authDomain: "woogie-project.firebaseapp.com",
@@ -21,15 +26,22 @@ const LoginUI = () => {
       console.log("Document data : ", docSnap.data());
       console.log(" data : ", docSnap.data().Email);
       console.log(" data : ", docSnap.data().Password);
+
+      setEmail(docSnap.data().Email);
+      setPassword(docSnap.data().Password);
+
+      Modal.success({ title: "조회 성공했습니다." });
+    } else {
+      Modal.error({ title: "조회에 실패했습니다." });
     }
   };
 
   return (
     <>
       <div>Firebase 데이터 불러오기</div>
-      이메일 : <span></span>
+      이메일 : <span>{Email}</span>
       <br />
-      비밀번호 : <span></span>
+      비밀번호 : <span>{Password}</span>
       <br />
       <button onClick={onClickFetchUser}>불러오기</button>
       <br />
